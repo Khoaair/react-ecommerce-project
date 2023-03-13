332a2de8eadb5cbdbdcd3ff5fa512e1289d9b9ec
KhoaAIr <KhoaAir@gmail.com>
2023-03-13 17:00:53 +0700

    filter context

--- src/components/FeaturedProducts.js
+++ src/components/FeaturedProducts.js
@@ -26,7 +26,7 @@ const FeaturedProducts = () => {
       </div>
       <div className='section-center featured'>
         {featured.slice(0, 3).map(product => {
-          return <Product key={product.key} {...product} />;
+          return <Product key={product.id} {...product} />;
         })}
       </div>
     </Wrapper>
