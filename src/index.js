b6f5db3669de4c550b304c939cb3af617d2ce342
KhoaAir win <KhoaAir@gmail.com>
2023-03-24 06:45:37 +0700

    auth0

--- src/index.js
+++ src/index.js
@@ -12,11 +12,20 @@ import { Auth0Provider } from '@auth0/auth0-react';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <ProductsProvider>
-    <FilterProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </FilterProvider>
-  </ProductsProvider>
+  <Auth0Provider
+    domain='{dev-gzzcm3v1m63q53un.us.auth0.com}'
+    clientId='{ROgEEXPZcfV1ex5k0v2pxZs7Ra4oG2bJ}'
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+    cacheLocation='localstorage'
+  >
+    <ProductsProvider>
+      <FilterProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </FilterProvider>
+    </ProductsProvider>
+  </Auth0Provider>
 );
